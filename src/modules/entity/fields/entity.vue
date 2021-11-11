69051fe79ea3ecb5541269f72678dc7ff5dd492f
Timofey <timoxa17@gmail.com>
2021-11-11 16:47:48 +0300

    entity field multiple

--- src/modules/entity/fields/entity.vue
+++ src/modules/entity/fields/entity.vue
@@ -2,7 +2,7 @@
 .form-field-entity
 	.form-field-entity-label
 		slot(name="label")
-	.form-field-entity-wrap.position-relative(
+	.form-field-entity-wrap(
 		@keypress.enter="selecting = true"
 		@keypress.space="selecting = true"
 		@keypress.escape="selecting = false"
@@ -10,23 +10,29 @@
 		tabindex="0"
 		:class="{ selecting }"
 	)
-		.entity-selection.px-2.py-1.rounded(
+		.entity-selection.ps-1.pe-3.py-1.rounded(
 			style="font-size:0.875rem"
 			@click.prevent="selecting = true"
 		)
-			div(v-for="item in modelValueArray")
-				.d-flex.align-items-center
-					span {{ getDisplayValue(item) }}
+			.px-1.text-muted(v-if="modelValueArray.length === 0") {{ placeholder || trans('nothingSelected') }}
+			ol.list-unstyled.m-0(v-else :class="multiple ? 'd-flex' : ''")
+				li.ps-1.me-1.d-flex(
+					v-for="(item, i) in modelValueArray" style="--bs-bg-opacity:0.25"
+					:class="multiple ? 'bg-secondary rounded' : ''"
+				)
+
+					.me-1.flex-grow-1 {{ getDisplayValue(item) }}
 					!=' '
-					//- .btn-close(type="button" @click.prevent="clearValue()")
-			.text-muted(v-if="modelValueArray.length === 0") {{ placeholder || trans('nothingSelected') }}
+					button.btn-entity-item-remove.rounded(
+						type="button"
+						@click.prevent.stop="removeItem(i)"
+					)
 			span.dropdown-toggle
-		.entity-select-dropdown.position-absolute.bg-light.rounded.top-0.left-0.pt-2(
+		.entity-select-dropdown.bg-light.rounded.pt-2(
 			v-show="selecting"
-			style="min-width:280px;width:100%;z-index:5;overflow:hidden"
 			v-click-outside="selecting ? onClickOutsideSelector : null"
 		)
-			.d-flex.justify-content-center(style="max-height:480px;width:100%")
+			.entity-select-dropdown-content
 				keep-alive
 					entity-view(
 						v-if="selecting"
@@ -37,7 +43,7 @@
 						v-model:perPage="perPage"
 						v-model:filters="filters"
 						v-model:sort="sort"
-						@item-click="setValue($event)"
+						@item-click="addItem($event)"
 					)
 </template>
 
@@ -97,8 +103,21 @@ export default defineComponent({
 		const filters = ref<Record<string, unknown>>({});
 		const sort = ref<Record<string, unknown>>({});
 		const selecting = ref(false);
-		const modelValueArray = computed(() => (Array.isArray(props.modelValue) ? props.modelValue : (props.modelValue && [props.modelValue]) || []));
+		const modelValueArray = computed<unknown[]>(() => {
+			if (Array.isArray(props.modelValue)) {
+				return props.modelValue;
+			}
+			return props.modelValue == null ? [] : [props.modelValue];
+		});
 		const internalRelatedItems = reactive<Record<string, unknown>>({});
+
+		const emitValue = (newValue: unknown[]) => {
+			if (props.multiple) {
+				emit('update:modelValue', newValue);
+			} else {
+				emit('update:modelValue', newValue.length > 0 ? newValue[newValue.length - 1] : null);
+			}
+		};
 
 		return {
 			...useTranslator(),
@@ -114,21 +133,24 @@ export default defineComponent({
 					selecting.value = false;
 				}
 			},
-			setValue(e: { id: string; item: Record<string, unknown> }) {
-				const oldValue = Array.isArray(props.modelValue) ? props.modelValue[0] : props.modelValue;
-				const newValue = props.key ? e.item[props.key] : e.id;
-				internalRelatedItems[e.id] = e.item;
-				selecting.value = false;
-				if (oldValue === newValue) {
+			removeItem(i: number) {
+				if (modelValueArray.value.length <= i) {
 					return;
 				}
-				emit('update:modelValue', props.multiple ? [newValue] : newValue);
+				const newValue = modelValueArray.value.slice();
+				newValue.splice(i, 1);
+				emitValue(newValue);
 			},
-			clearValue() {
-				if (props.modelValue === null) {
+			addItem(e: { id: string; item: Record<string, unknown> }) {
+				if (!props.multiple) {
+					selecting.value = false;
+				}
+				const item = props.key ? e.item[props.key] : e.id;
+				if (modelValueArray.value.includes(item)) {
 					return;
 				}
-				emit('update:modelValue', props.multiple ? [] : null);
+				internalRelatedItems[e.id] = e.item;
+				emitValue([...modelValueArray.value, item]);
 			},
 			onClickOutsideSelector() {
 				selecting.value = false;
@@ -147,6 +169,7 @@ export default defineComponent({
 
 <style lang="stylus" scoped>
 .form-field-entity-wrap
+	position relative
 	&:focus
 		outline 0
 .entity-selection
@@ -160,13 +183,52 @@ export default defineComponent({
 	border 1px solid #86b7fe
 	box-shadow unquote('0 0 0 0.25rem rgba(var(--bs-primary-rgb), 0.25)')
 .entity-select-dropdown
+	position absolute
+	top 0
+	left 0
+	right 0
+	min-width 280px
+	z-index 5
+	overflow hidden
 	animation entity-dropdown-appear 0.1s ease-out
+.entity-select-dropdown-content
+	display flex
+	justify-content center
+	max-height 480px
+	width 100%
 .dropdown-toggle
 	position absolute
 	top 50%
 	right 0.5rem
 	margin auto
 	transform translateY(-50%)
+.btn-entity-item-remove
+	display block
+	width 1.5em
+	position relative
+	background unquote('rgba(var(--bs-danger-rgb), 0.8)')
+	border none
+	padding 0
+	margin 0
+	color white
+	&:hover
+		background var(--bs-danger)
+	&::before, &::after
+		content ''
+		display block
+		border-top 1px solid
+		border-bottom @border-top
+		border-radius 2px
+		position absolute
+		top 50%
+		right 0.25rem
+		left 0.25rem
+		margin-top -1px
+		backface-visibility hidden
+	&::before
+		transform rotate(45deg)
+	&::after
+		transform rotate(-45deg)
 @keyframes entity-dropdown-appear
 	0%
 		opacity 0
