95d3b87010022fd2f2be650cbbfdea5d43563013
Timofey <timoxa17@gmail.com>
2021-11-19 15:34:32 +0300

    ignore demo files

--- src/demo-app.ts
+++ src/demo-app.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import App from './main';
+// @ts-ignore
 import bootstrap from './app';
 
 const app = new App();
+// @ts-ignore
 bootstrap(app.getIocContainer());
 app.mount('#app');
