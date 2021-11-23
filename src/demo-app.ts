983bb942e72a003dec91392e1fa4dd13c43175d7
Timofey <timoxa17@gmail.com>
2021-11-23 20:02:44 +0300

    demo-app improvements

--- src/demo-app.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-import App from './main';
-// @ts-ignore
-import bootstrap from './app';
-
-const app = new App();
-// @ts-ignore
-bootstrap(app.getIocContainer());
-app.mount('#app');
