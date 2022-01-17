5a7d80a1bbc0f95c1dd9487ea87b76bff097bafe
Timofey <timoxa17@gmail.com>
2022-01-17 22:09:48 +0300

    build to separate module set

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,20 @@ import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 import { dependencies } from './package.json';
 
-const plugins = [vue(), minifyHtml()];
-
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-	plugins: command === 'build' ? [...plugins, dts()] : plugins,
+	plugins: [
+		vue(),
+		minifyHtml(),
+		command === 'build' &&
+			dts({
+				beforeWriteFile(path) {
+					return {
+						filePath: path.replace('/dist/src/', '/dist/'),
+					};
+				},
+			}),
+	],
 	server: {
 		proxy: {
 			'/api': process.env.API_BACKEND || 'http://localhost:8000',
@@ -18,20 +27,22 @@ export default defineConfig(({ command }) => ({
 	build: command === 'build' && {
 		lib: {
 			entry: resolve(__dirname, 'src/main.ts'),
-			name: 'VueAdmin',
+			formats: ['es'],
 		},
 		rollupOptions: {
 			external: Object.keys(dependencies),
-			output: [
-				{
-					format: 'esm',
+			preserveModules: true,
+			output: {
+				entryFileNames(chunk) {
+					if (chunk.facadeModuleId.endsWith('.vue')) {
+						return `${chunk.name}.vue.js`;
+					}
+					if (chunk.facadeModuleId.includes('.vue?vue')) {
+						return `${chunk.name.split('.vue_vue')[0]}.vue.${chunk.facadeModuleId.split('.vue?vue')[1].split('&type=')[1].split('&')[0]}.js`;
+					}
+					return `${chunk.name}.js`;
 				},
-				{
-					format: 'umd',
-					inlineDynamicImports: true,
-					exports: 'named',
-				},
-			],
+			},
 		},
 	},
 }));
