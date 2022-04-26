d9877114b76513b281e22f8231bc0b759b017f55
Timofey <timoxa17@gmail.com>
2022-04-26 17:09:18 +0300

    quill wysiwyg field added

---
+++ src/modules/form/fields/wysiwyg.vue
@@ -0,0 +1,74 @@
+<template lang="pug">
+.form-field-wysiwyg
+	slot(name="label")
+	.border.rounded
+		div(ref="editorEl" v-html="modelValue")
+	.invalid-feedback(v-if="errors && errors.length")
+		div(v-for="err in errors") {{ err }}
+</template>
+
+<script lang="ts">
+import type { PropType } from 'vue';
+import { onBeforeUnmount, defineComponent, ref, watch } from 'vue';
+import type { QuillOptionsStatic } from 'quill';
+import Quill from 'quill';
+import 'quill/dist/quill.core.css';
+import 'quill/dist/quill.snow.css';
+import { getCommonProps } from './common';
+
+export type WysiwygComponentConfig = QuillOptionsStatic;
+
+export default defineComponent({
+	props: {
+		...getCommonProps({
+			type: String,
+			default: '',
+		}),
+		config: {
+			type: Object as PropType<WysiwygComponentConfig>,
+			default: () => ({
+				theme: 'snow',
+				formats: ['bold', 'italic', 'link', 'strike', 'script', 'underline', 'blockquote', 'header', 'list'],
+				modules: {
+					toolbar: [
+						[{ header: [2, 3, false] }],
+						['bold', 'italic', 'underline', 'strike', 'link'],
+						['blockquote'],
+						[{ list: 'ordered' }, { list: 'bullet' }],
+						[{ script: 'sub' }, { script: 'super' }],
+						['clean'],
+					],
+				},
+			}),
+		},
+	},
+	emits: ['update:modelValue'],
+	setup(props, { emit }) {
+		const editorEl = ref<HTMLDivElement | null>(null);
+		let updateTimeout = -1;
+
+		watch(editorEl, () => {
+			if (!editorEl.value) {
+				return;
+			}
+			const quill = new Quill(editorEl.value, props.config);
+			quill.on('text-change', () => {
+				clearTimeout(updateTimeout);
+				updateTimeout = setTimeout(() => {
+					if (!editorEl.value) {
+						return;
+					}
+					emit('update:modelValue', editorEl.value.innerHTML);
+				}, 200);
+			});
+		});
+		onBeforeUnmount(() => {
+			clearTimeout(updateTimeout);
+		});
+
+		return { editorEl };
+	},
+});
+</script>
+
+<style lang="stylus" scoped></style>
