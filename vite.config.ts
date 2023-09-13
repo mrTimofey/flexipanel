003f1f8cd26bd0f4681d6f96614d3ea530460479
Timofey <timoxa17@gmail.com>
2023-09-13 19:03:22 +0300

    improve vue files build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+import type { UserConfigExport } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import { dependencies } from './package.json';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
 	plugins: [
 		vue(),
 		createHtmlPlugin(),
@@ -17,6 +17,7 @@ export default defineConfig({
 		},
 	],
 	build: {
+		target: 'esnext',
 		lib: {
 			entry: 'src/main.ts',
 			formats: ['es'],
@@ -24,22 +25,24 @@ export default defineConfig({
 		rollupOptions: {
 			external: Object.keys(dependencies),
 			output: {
-				// remove src prefix in output file paths
 				preserveModulesRoot: 'src',
 				preserveModules: true,
 				// Rename emitted Vue component sources so they named `[name].vue.js`.
 				// It is a convenient way to allow applications importing them like '.../name.vue' when using bundlers.
 				// Also `.d.ts` files are generated as `[name].vue.d.ts` so we do not need to tweak them - everything just works.
 				entryFileNames(chunk) {
-					if (chunk.facadeModuleId?.endsWith('.vue')) {
-						return `${chunk.name}.vue.js`;
+					if (!chunk.name.endsWith('.vue')) {
+						return `${chunk.name}.js`;
 					}
-					if (chunk.facadeModuleId?.includes('.vue?vue')) {
-						return `${chunk.name.split('.vue_vue')[0]}.vue.${chunk.facadeModuleId.split('.vue?vue')[1].split('&type=')[1].split('&')[0]}.js`;
-					}
-					return `${chunk.name}.js`;
+					// Vue plugin emits multiple chunks for each vue component: main entry and style/template/script blocks
+					// main entry is placed to its original folder, other files are going to their own separate folder
+					const vueChunkParams = (chunk.facadeModuleId ?? chunk.moduleIds[0] ?? '').split('?')[1] ?? '';
+					return vueChunkParams
+						? // TODO: merge all chunks into a single file
+						  `_vue-chunks/[hash].js`
+						: `${chunk.name}.js`;
 				},
 			},
 		},
 	},
-});
+} satisfies UserConfigExport;
